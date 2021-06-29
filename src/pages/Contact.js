d48608783f2df344bf9c72d7a110cbe70f186334
green-applesauce <70298029+green-applesauce@users.noreply.github.com>
2021-06-28 17:50:28 -0700

    stats and contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Main from '../layouts/Main';
-import EmailLink from '../components/Contact/EmailLink';
 import ContactIcons from '../components/Contact/ContactIcons';
 
 const Contact = () => (
@@ -17,8 +16,7 @@ const Contact = () => (
         </div>
       </header>
       <div className="email-at">
-        <p>Feel free to get in touch. You can email me at: </p>
-        <EmailLink />
+        <p>Get in touch! You can email me at: <a href="mailto:pinicolei@gmail.com">pinicolei@gmail.com</a></p>
       </div>
       <ContactIcons />
     </article>
